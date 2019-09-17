b87937f9f8f6b50ea65030469865408b34e24f41
Azat Nizameev <azat.nizam@gmail.com>
2019-09-17 18:45:20 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+ymaps.ready(init);
+
+function init () {
+    var fujidaMap = new ymaps.Map('map', {
+            center: [53.70, 47.00],
+            zoom: 5
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        objectManager = new ymaps.ObjectManager({
+            // Кластеризвция меток
+            clusterize: true,
+            gridSize: 32,
+            clusterDisableClickZoom: true
+        });
+
+    // Вид маркеров
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+    fujidaMap.geoObjects.add(objectManager);
+
+    $.ajax({
+        url: "address.json"
+    }).done(function(data) {
+        objectManager.add(data);
+    });
+
+}
